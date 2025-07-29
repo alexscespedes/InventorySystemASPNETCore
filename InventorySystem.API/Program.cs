a332d77593245c131c65d63e80919b79ee10f862
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-07-29 19:21:05 -0400

    Seed Sample Data & Test with Swagger

--- InventorySystem.API/Program.cs
+++ InventorySystem.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using InventorySystem.Infrastructure.Data;
+using InventorySystem.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,4 +29,33 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+SeedDatabase(app);
+
 app.Run();
+
+void SeedDatabase(IHost app)
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<InventoryDbContext>();
+
+    if (!context.Products.Any())
+    {
+        var product = new Product { Name = "Laptop", SKU = "LP-123", Description = "Portable Computer" };
+        var warehouse = new Warehouse { Name = "Main Warehouse", Location = "New York" };
+
+        context.Products.Add(product);
+        context.Warehouses.Add(warehouse);
+        context.SaveChanges();
+
+        var stock = new StockTransaction
+        {
+            ProductId = product.Id,
+            WarehouseId = warehouse.Id,
+            Quanity = 20,
+            Timestamp = DateTime.UtcNow
+        };
+
+        context.StockTransactions.Add(stock);
+        context.SaveChanges();
+    }
+}
